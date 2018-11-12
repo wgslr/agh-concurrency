c613b311e3c6e5cc060e0f1c1047e4d248bf1e20
Wojciech Geisler <wojciech.geisler@gmail.com>
2018-11-12 16:04:37 +0100

    Multiple trials in the timing script

--- time.sh
+++ time.sh
@@ -1,12 +1,18 @@
 #!/bin/bash -e
 
+TMP=/tmp/time.csv
 TIME=$(date '+%Y%m%dT%H%M%S')
 PREFIX="$(pwd)/./times/${TIME}_"
+REPEATS=10
 
 cd out/production/lab4b/
-
-for m in {10,10000,100000}; do
-  for pk in {10,100,1000}; do
-    java ProdConsApp $pk $m > ${PREFIX}${pk}_${m}
+for _ in {1..$REPEATS}; do
+  for m in {10,10000,100000}; do
+    for pk in {10,100,1000}; do
+      java ProdConsApp $pk $m > $TMP
+      cat $TMP >> ${PREFIX}${pk}_${m}.csv
+    done
   done
 done
+
+rm $TMP || true
