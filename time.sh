86ef8c109940ccf7e11d7acac8b246815b7a3a58
Wojciech Geisler <wojciech.geisler@gmail.com>
2018-11-12 17:03:19 +0100

    Add run params to csv

--- time.sh
+++ time.sh
@@ -2,17 +2,20 @@
 
 TMP=/tmp/time.csv
 TIME=$(date '+%Y%m%dT%H%M%S')
-PREFIX="$(pwd)/times/${TIME}/"
+FILE="$(pwd)/times/${TIME}.csv"
 REPEATS=10
 
-mkdir -p "$PREFIX"
+#mkdir -p "$PREFIX"
 
 cd out/production/lab4b/
 for _ in {1..$REPEATS}; do
   for m in {10,10000,100000}; do
     for pk in {10,100,1000}; do
-      java ProdConsApp $pk $m | grep -v start > $TMP
-      cat $TMP >> ${PREFIX}${pk}_${m}.csv
+      echo Size $m Workers $pk: START
+      java ProdConsApp $pk $m | grep -v start | cut -d ';' -f 2-5 > $TMP
+      LINES=$(wc -l $TMP | cut -d ' ' -f 1)
+      paste <(yes "$pk; $m; " | head -n $LINES) $TMP >> $FILE # ${PREFIX}${pk}_${m}.csv
+      echo Size $m Workers $pk: DONE
     done
   done
 done
