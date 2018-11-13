81e5806664f9aa3c281f95101b4b2e74fbecd39c
Wojciech Geisler <wojciech.geisler@gmail.com>
2018-11-13 22:22:52 +0100

    Remove debug printouts

--- src/FairBuffer.java
+++ src/FairBuffer.java
@@ -34,20 +34,12 @@ public class FairBuffer extends AbstractBuffer {
             while (freeSpace() < portions || !(
                     id.equals(putClients.peek()) || contentCount <= size / 2
             )) {
-
-//                System.out.println("Waiting for " + id + ", put queue is " +
-//                        putClients.stream()
-//                                .map(Objects::toString)
-//                                .collect(Collectors.joining(" ")));
-
                 if (!contentChanged.await(ProdConsApp.LOCK_TIMEOUT_SECONDS, TimeUnit.SECONDS)) {
-                    System.out.println("Put " + id + " of " + portions + " timeout at queue " +  queueToStr(putClients));
                     return;
                 }
             }
 
             Long popped = getClients.poll();
-            System.out.println("Putting cause queue front is " + popped);
             assert id.equals(popped);
 
             contentCount += portions;
@@ -59,39 +51,23 @@ public class FairBuffer extends AbstractBuffer {
         }
     }
 
-    private String queueToStr(Queue<Long> l) {
-        return l.stream()
-                .map(Objects::toString)
-                .collect(Collectors.joining(" "));
-    }
-
     @Override
     public void do_get(long portions) {
         lock.lock();
 
         try {
             Long id = makeOperationId();
-            System.out.println("My id is " + id);
-
             getClients.add(id);
-            System.out.println("Added id " + id + " to get queue: " + queueToStr(getClients));
-
 
             while (contentCount < portions || !(id.equals(getClients.peek())
                     || contentCount > size / 2
             )) {
-
-//                System.out.println(
-//                        "Waiting for " + id + ", get queue is " + queueToStr(getClients));
-
                 if (!contentChanged.await(ProdConsApp.LOCK_TIMEOUT_SECONDS, TimeUnit.SECONDS)) {
-                    System.out.println("Get " + id + " of " + portions + " timeout at queue " + queueToStr(getClients));
                     return;
                 }
             }
 
             Long popped = getClients.poll();
-            System.out.println("Getting cause queue front is " + popped);
             assert id.equals(popped);
 
             contentCount -= portions;
@@ -112,5 +88,11 @@ public class FairBuffer extends AbstractBuffer {
 
     private Long makeOperationId() {
         return nextId.incrementAndGet();
+    }
+
+    private String queueToStr(Queue<Long> l) {
+        return l.stream()
+                .map(Objects::toString)
+                .collect(Collectors.joining(" "));
     }
 }
