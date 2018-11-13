b2250b195262a62f63ad99ab0c9cf0bd6433245a
Wojciech Geisler <wojciech.geisler@gmail.com>
2018-11-13 21:50:45 +0100

    Add repeats and timeout to timer script

--- time.sh
+++ time.sh
@@ -3,16 +3,19 @@
 TMP=/tmp/time.csv
 TIME=$(date '+%Y%m%dT%H%M%S')
 FILE="$(pwd)/times/${TIME}.csv"
-REPEATS=10
+REPEATS=200
+TIMEOUT=60
 
-#mkdir -p "$PREFIX"
+
+echo $TIME
 
 cd out/production/lab4b/
-for _ in {1..$REPEATS}; do
+
+for ((n=0;n<$REPEATS;n++)); do
   for m in {10,10000,100000}; do
     for pk in {10,100,1000}; do
       echo Size $m Workers $pk: START
-      java ProdConsApp $pk $m | grep -v start | cut -d ';' -f 2-5 > $TMP
+      timeout $TIMEOUT java ProdConsApp $pk $m | grep -v start | cut -d ';' -f 2-5 > $TMP
       LINES=$(wc -l $TMP | cut -d ' ' -f 1)
       paste <(yes "$pk; $m; " | head -n $LINES) $TMP >> $FILE # ${PREFIX}${pk}_${m}.csv
       echo Size $m Workers $pk: DONE
