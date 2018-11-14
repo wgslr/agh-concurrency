3b2a861bfb45f0b8a917238ca59c68fcafcf1e3d
Wojciech Geisler <wojciech.geisler@gmail.com>
2018-11-14 15:25:28 +0100

    Generate some image on canvas

--- src/MandelbrotApp.java
+++ src/MandelbrotApp.java
@@ -1,16 +1,14 @@
 public class MandelbrotApp {
     public static void main(String argv[]) {
 //
-//        double vals[][] = new double[10][10];
-//        int color[] = new int[10*10];
-//        System.out.println(vals.length);
-//        for (int y = 0; y < vals.length; y++) {
-//            for (int x = 0; x < vals[y].length; x++) {
-//                vals[y][x] = x * y;
-//                color[vals[y].length * y + x] = x * y % 255;
-//            }
-//        }
+        int color[][] = new int[500][500];
+        System.out.println(color.length);
+        for (int y = 0; y < color.length; y++) {
+            for (int x = 0; x < color[y].length; x++) {
+                color[y][x] = x * y % 255;
+            }
+        }
 
-//        new Canvas(vals, color).setVisible(true);
+        new Canvas(color).setVisible(true);
     }
 }
