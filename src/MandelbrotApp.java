9a9f86825fe0937abce95fdabe1a84011e940e58
Wojciech Geisler <wojciech.geisler@gmail.com>
2018-11-14 15:43:20 +0100

    Make pixel computation static

--- src/MandelbrotApp.java
+++ src/MandelbrotApp.java
@@ -1,11 +1,11 @@
 public class MandelbrotApp {
     public static void main(String argv[]) {
 //
-        int color[][] = new int[500][500];
+        int color[][] = new int[600][800];
         System.out.println(color.length);
         for (int y = 0; y < color.length; y++) {
             for (int x = 0; x < color[y].length; x++) {
-                color[y][x] = x * y % 255;
+                color[y][x] = Mandelbrot.colorForCoords(x,y);
             }
         }
 
