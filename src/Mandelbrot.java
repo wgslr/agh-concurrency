922e6c59eda769ef1b4b77423293c8439f3d701a
Wojciech Geisler <wojciech.geisler@gmail.com>
2018-11-14 15:46:39 +0100

    Adjust zoom and offset to better fill area

--- src/Mandelbrot.java
+++ src/Mandelbrot.java
@@ -3,13 +3,13 @@ import javax.swing.JFrame;
 
 public class Mandelbrot extends JFrame {
     private static final int MAX_ITER = 570;
-    private static final double ZOOM = 150;
+    private static final double ZOOM = 200;
 
     public static int colorForCoords(int x, int y) {
         double zx, zy, cX, cY, tmp;
 
         zx = zy = 0;
-        cX = (x - 400) / ZOOM;
+        cX = (x - 500) / ZOOM;
         cY = (y - 300) / ZOOM;
         int iter = MAX_ITER;
         while (zx * zx + zy * zy < 4 && iter > 0) {
