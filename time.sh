2a2a1a845e5df2c4d6b8bb16067b8a3ba59f53a1
Wojciech Geisler <wojciech.geisler@gmail.com>
2018-11-16 01:11:53 +0100

    Add timing script

---
+++ time.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+
+TMP=/tmp/time.csv
+TMP2=/tmp/time2.csv
+TIME=$(date '+%Y%m%dT%H%M%S')
+FILE="$(pwd)/times/${TIME}.csv"
+REPEATS=100
+TIMEOUT=60
+
+
+echo $TIME
+
+cd out/production/lab5/
+
+for ((n=0;n<$REPEATS;n++)); do
+  timeout $TIMEOUT java MandelbrotApp > $TMP || true
+  cat $TMP | grep -v Start > $TMP2
+  cat $TMP2 >> $FILE # ${PREFIX}${pk}_${m}.csv
+  echo Iteration $n DONE $((REPEATS - n)) LEFT
+done
+
+rm $TMP || true
