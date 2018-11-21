66a1a692c1f804be1b6d95148d600bc4def8a9d5
Wojciech Geisler <wojciech.geisler@gmail.com>
2018-11-21 16:00:30 +0100

    Build stacks from the word

--- foat.hs
+++ foat.hs
@@ -1,21 +1,29 @@
 import qualified Data.Map as Map
 
-data Rel = Dep | Indep deriving Show
+data Rel = Dep | Indep deriving (Show, Eq)
 
-rel x y | x == y  = Dep
+alphabet = "abcd"
+
+rel x y 
+  | x == y  = Dep
+  | not (elem x alphabet && elem y alphabet) = error ("Unknown relation for letters "  ++ [x] ++ " " ++ [y])
 rel 'a' 'b' = Dep
 rel 'a' 'c' = Dep
 rel 'a' 'd' = Indep
 rel 'b' 'c' = Indep
 rel 'b' 'd' = Dep
 rel 'c' 'd' = Dep
-rel x y =
-  case rel y x of
-    Indep -> Dep
-    Dep -> Indep
-
+rel x y = rel y x
   
 genFoat word = buildStacks' (word) 
 
 buildStacks' :: [Char] -> (Map.Map Char [Char])
-buildStacks' word = foldl (\stacks  l -> Map.insertWith (++) l [l] stacks) Map.empty (reverse word)
+buildStacks' word = foldl (
+    \stacks  l -> 
+      foldl (\stacks' alph -> if l == alph 
+          then Map.insertWith (++) l [l] stacks'
+          else (if rel l alph == Dep then (Map.insertWith (++) alph "*" stacks')  else stacks')
+        ) stacks  alphabet
+  ) (initStacks word) (reverse word)
+
+initStacks = foldl (\m l -> Map.insert l [] m) Map.empty
