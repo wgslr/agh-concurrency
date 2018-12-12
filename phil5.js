d29addbef857dc087af50f9d1f72330d02984e6b
Wojciech Geisler <wojciech.geisler@gmail.com>
2018-12-12 16:09:32 +0100

    Add 5 philosophers with waiter

--- phil5.js
+++ phil5.js
@@ -1,22 +1,24 @@
 const EXP = 1.5;
 const MAX_EAT = 500;
 
+let totalBackoff = 0;
+
 const Fork = function () {
     this.state = 0;
     return this;
 }
 
 function beb(pred, action, backoff) {
-    setTimeout(() =>{
+    setTimeout(() => {
         if (pred()) {
-        action();
-    } else {
-        // console.log(`Predicate ${pred} false`);
-        beb(pred, action, backoff * EXP);
-    }
+            action();
+        } else {
+            backoff *= EXP;
+            totalBackoff += backoff;
+            beb(pred, action, backoff);
+        }
     }, Math.random() * backoff);
 }
-
 
 Fork.prototype.acquire = function (cb) {
     beb(() => this.state == 0,
@@ -24,11 +26,30 @@ Fork.prototype.acquire = function (cb) {
             this.state = 1;
             cb();
         },
-        10);
+        1);
 }
 
 Fork.prototype.release = function () {
     this.state = 0;
+}
+
+const Waiter = function () {
+    this.count = 0;
+    return this;
+}
+
+Waiter.prototype.free = function (cb) {
+    this.count--;
+}
+
+Waiter.prototype.acquire = function (cb) {
+    beb(() => this.count < 4,
+        () => {
+            this.count++;
+            console.log(`waiter count ${this.count}`);
+            cb();
+        },
+        1);
 }
 
 const Philosopher = function (id, forks) {
@@ -39,30 +60,30 @@ const Philosopher = function (id, forks) {
     return this;
 }
 
-Philosopher.prototype.startNaive = function (count) {
-    const f1 = this.forks[this.f1];
-    const f2 = this.forks[this.f2];
-    const id = this.id;
+// Philosopher.prototype.startNaive = function (count) {
+//     const f1 = this.forks[this.f1];
+//     const f2 = this.forks[this.f2];
+//     const id = this.id;
 
-    if (count <= 0) {
-        return;
-    }
+//     if (count <= 0) {
+//         return;
+//     }
 
-    f1.acquire(() => {
-        console.log(`${id} got fork ${this.f1}`);
-        f2.acquire(() => {
-            console.log(`${id} got fork ${this.f2}`);
-            const eatTime = Math.random() * MAX_EAT;
-            console.log(`${id} eating for ${eatTime}`);
-            setTimeout(() => {
-                console.log(`${id} finished`);
-                f1.release();
-                f2.release();
-                this.startNaive(count - 1);
-            }, eatTime);
-        })
-    });
-}
+//     f1.acquire(() => {
+//         console.log(`${id} got fork ${this.f1}`);
+//         f2.acquire(() => {
+//             console.log(`${id} got fork ${this.f2}`);
+//             const eatTime = Math.random() * MAX_EAT;
+//             console.log(`${id} eating for ${eatTime}`);
+//             setTimeout(() => {
+//                 console.log(`${id} finished`);
+//                 f1.release();
+//                 f2.release();
+//                 this.startNaive(count - 1);
+//             }, eatTime);
+//         })
+//     });
+// }
 
 Philosopher.prototype.startAsym = function (count) {
     const id = this.id;
@@ -74,30 +95,47 @@ Philosopher.prototype.startAsym = function (count) {
     }
 
     f1.acquire(() => {
-        console.log(`${id} got fork ${this.f1}`);
+        // console.log(`asym ${id} got fork ${this.f1}`);
         f2.acquire(() => {
-            console.log(`${id} got fork ${this.f2}`);
+            // console.log(`asym ${id} got fork ${this.f2}`);
             const eatTime = Math.random() * MAX_EAT;
-            console.log(`${id} eating for ${eatTime}`);
+            // console.log(`asym ${id} eating for ${eatTime}`);
             setTimeout(() => {
-                console.log(`${id} finished`);
+                console.log(`asym ${id} finished ${11 - count}th time`);
                 f1.release();
                 f2.release();
-                this.startNaive(count - 1);
+                this.startAsym(count - 1);
             }, eatTime);
         })
     });
 }
 
-Philosopher.prototype.startConductor = function (count) {
-    var forks = this.forks,
-        f1 = this.f1,
-        f2 = this.f2,
-        id = this.id;
+Philosopher.prototype.startConductor = function (count, waiter) {
+    const f1 = this.forks[this.f1];
+    const f2 = this.forks[this.f2];
+    const id = this.id;
 
-    // zaimplementuj rozwiazanie z kelnerem
-    // kazdy filozof powinien 'count' razy wykonywac cykl
-    // podnoszenia widelcow -- jedzenia -- zwalniania widelcow
+    if (count <= 0) {
+        return;
+    }
+
+    waiter.acquire(() => {
+        f1.acquire(() => {
+            // console.log(`wait ${id} got fork ${this.f1}`);
+            f2.acquire(() => {
+                // console.log(`wait ${id} got fork ${this.f2}`);
+                const eatTime = Math.random() * MAX_EAT;
+                // console.log(`wait ${id} eating for ${eatTime}`);
+                setTimeout(() => {
+                    console.log(`wait ${id} finished ${11 - count}th time`);
+                    f1.release();
+                    f2.release();
+                    waiter.free();
+                    this.startConductor(count - 1, waiter);
+                }, eatTime);
+            })
+        });
+    });
 }
 
 
@@ -116,3 +154,14 @@ for (var i = 0; i < N; i++) {
     // philosophers[i].startNaive(10);
     philosophers[i].startAsym(10);
 }
+setTimeout(() => {
+    console.log(`asym; ${totalBackoff}`);
+    console.log("\n\n");
+    const waiter = new Waiter();
+    for (var i = 0; i < N; i++) {
+        philosophers[i].startConductor(10, waiter);
+    }
+    setTimeout(() => {
+        console.log(`waiter; ${totalBackoff}`);
+    }, 25000);
+}, 25000);
