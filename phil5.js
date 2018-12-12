11484cfa2ae3fc4ccd46c5ad3763ba04e6335c69
Wojciech Geisler <wojciech.geisler@gmail.com>
2018-12-12 15:29:32 +0100

    Obtain deadlock

--- phil5.js
+++ phil5.js
@@ -1,18 +1,30 @@
-const BACKOFF = 10;
-const EXP = 2;
-const MAX_EAT = 3000;
+const EXP = 1.5;
+const MAX_EAT = 500;
 
 var Fork = function () {
     this.state = 0;
     return this;
 }
 
+function beb(pred, action, backoff) {
+    setTimeout(() => {
+        if (pred()) {
+            action();
+        } else {
+            // console.log(`Predicate ${pred} false`);
+            beb(pred, action, backoff * EXP);
+        }
+    }, backoff);
+}
+
+
 Fork.prototype.acquire = function (cb) {
-    // zaimplementuj funkcje acquire, tak by korzystala z algorytmu BEB
-    // (http://pl.wikipedia.org/wiki/Binary_Exponential_Backoff), tzn:
-    // 1. przed pierwsza proba podniesienia widelca Filozof odczekuje 1ms
-    // 2. gdy proba jest nieudana, zwieksza czas oczekiwania dwukrotnie
-    //    i ponawia probe itd.
+    beb(() => this.state == 0,
+        () => {
+            this.state = 1;
+            cb();
+        },
+        1);
 }
 
 Fork.prototype.release = function () {
@@ -27,16 +39,6 @@ var Philosopher = function (id, forks) {
     return this;
 }
 
-function beb(pred, action, backoff) {
-    if (pred()) {
-        action();
-    } else {
-        console.log(`Predicate ${pred} false`);
-        backoff *= EXP;
-        setTimeout(beb, backoff, pred, action, backoff);
-    }
-}
-
 Philosopher.prototype.startNaive = function (count) {
     const f1 = this.forks[this.f1];
     const f2 = this.forks[this.f2];
@@ -46,27 +48,20 @@ Philosopher.prototype.startNaive = function (count) {
         return;
     }
 
-    beb(() => f1.state == 0,
-        () => {
-            f1.state == 1;
-            console.log(`${id} got fork 1`);
-            beb(() => f2.state == 0,
-                () => {
-                    console.log(`${id} got fork 2`);
-                    f2.state = 1;
-                    const eatTime = Math.random() * MAX_EAT;
-                    console.log(`${id} eating for ${eatTime}`);
-                    setTimeout(() => {
-                        console.log(`${id} finished`);
-                        f1.release();
-                        f2.release();
-                        this.startNaive(count - 1);
-                    }, eatTime);
-                },
-                BACKOFF
-            )
-        },
-        BACKOFF);
+    f1.acquire(() => {
+        console.log(`${id} got fork ${this.f1}`);
+        f2.acquire(() => {
+            console.log(`${id} got fork ${this.f2}`);
+            const eatTime = Math.random() * MAX_EAT;
+            console.log(`${id} eating for ${eatTime}`);
+            setTimeout(() => {
+                console.log(`${id} finished`);
+                f1.release();
+                f2.release();
+                this.startNaive(count - 1);
+            }, eatTime);
+        })
+    });
 
     // zaimplementuj rozwiazanie naiwne
     // kazdy filozof powinien 'count' razy wykonywac cykl
@@ -108,5 +103,5 @@ for (var i = 0; i < N; i++) {
 }
 
 for (var i = 0; i < N; i++) {
-    philosophers[i].startNaive(10);
+    philosophers[i].startNaive(100);
 }
